d8994a15cd786f64b27f36bec80e966d1b4939c2
necudeco <necudeco@gmail.com>
2020-03-31 13:36:33 -0500

    31 Marzo

---
+++ js/data.js
@@ -0,0 +1,323 @@
+const data = {
+
+    '2020-03-06': {
+        muestras: 0,
+        infectados: 1,
+        muestras: 0
+    },
+
+    '2020-03-07': {
+        muestras: 0,
+        infectados: 6,
+        muestras: 0
+    },
+
+    '2020-03-08': {
+        muestras: 0,
+        infectados: 7,
+        muestras: 0
+    },
+
+    '2020-03-11': {
+        muestras: 0,
+        infectados: 17,
+        muestras: 0
+    },
+
+    '2020-03-12': {
+        muestras: 0,
+        infectados: 22,
+        muestras: 0
+    },
+
+    '2020-03-13': {
+        muestras: 0,
+        infectados: 38,
+        muestras: 0
+    },
+
+    '2020-03-14': {
+        muestras: 0,
+        infectados: 46,
+        muestras: 0
+    },
+
+    '2020-03-15': {
+        muestras: 0,
+        infectados: 71,
+        muestras: 0
+    },
+
+    '2020-03-16': {
+        muestras: 0,
+        infectados: 86,
+        muestras: 0
+    },
+
+    '2020-03-17': {
+        muestras: 0,
+        infectados: 117,
+        muestras: 0
+    },
+
+
+    '2020-03-18': {
+        muestras: 0,
+        infectados: 145,
+        muestras: 0
+    },
+
+    '2020-03-19': {
+        muestras: 0,
+        infectados: 234,
+        muestras: 0
+    },
+
+    '2020-03-20': {
+        muestras: 0,
+        infectados: 263,
+        muestras: 0
+    },
+
+    '2020-03-21': {
+        muestras: 0,
+        infectados: 318,
+        muestras: 0
+    },
+
+    '2020-03-22': {
+        muestras: 0,
+        infectados: 363,
+        muestras: 0
+    },
+
+    '2020-03-23': {
+        muestras: 0,
+        infectados: 395,
+        muestras: 0
+    },
+
+    '2020-03-24': {
+        muestras: 0,
+        infectados: 416,
+        muestras: 0
+    },
+
+    '2020-03-25': {
+        muestras: 0,
+        infectados: 480,
+        muestras: 0
+    },
+
+    '2020-03-26': {
+        muestras: 0,
+        infectados: 580,
+        muestras: 0
+    },
+
+    '2020-03-27': {
+        muestras: 0,
+        infectados: 671,
+        muestras: 0
+    },
+
+    '2020-03-28': {
+        muestras: 0,
+        infectados: 852,
+        muestras: 0
+    },
+
+    '2020-03-30': {
+        muestras: 12502,
+        infectados: 950,
+        muertos: 18,
+        pordepartamento:{
+            lima: {
+                infectados: 518,
+                muestras: 8000
+            },
+            lambayeque:{
+                infectados: 15,
+                muestras: 0
+            },
+            callao:{
+                infectados:18,
+                muestras: 0
+            },
+            arequipa:{
+                infectados:15,
+                muestras: 0,
+            },
+            huanuco:{
+                infectados: 4,
+                muestras: 0
+            },
+            piura:{
+                infectados: 20,
+                muestras: 0
+            },
+            ancash: {
+                infectados: 6,
+                muestras: 0
+            },
+            cuzco: {
+                infectados: 11,
+                muestras: 0,
+            },
+            ica:{
+                infectados:4, 
+                muestras: 0,
+            },
+            lalibertad: {
+                infectados: 12,
+                muestras: 0
+            },
+            loreto:{
+                infectados: 27,
+                muestras: 0
+            },
+            madrededios:{
+                infectados: 1,
+                muestras: 0
+            },
+            junin: {
+                infectados: 12,
+                muestras: 0
+            },
+            tumbes: {
+                infectados: 6, 
+                muestras: 0
+            },
+            sanmartin: {
+                infectados: 2, 
+                muestras: 0,
+            },
+            cajamarca: {
+                infectados: 1,
+                muestras: 0
+            },
+            pasco: {
+                infectados: 1,
+                muestras: 0
+            }
+        }
+    },
+
+    '2020-03-31': {
+        muestras: 14463,
+        infectados: 1065,
+        hospitalizados: 190,
+        recuperados: 394,
+        muertos: 24,
+        detenidos: 36000,
+        pordepartamento:{
+            lima: {
+                infectados: 799,
+                muestras: 8000
+            },
+            lambayeque:{
+                infectados: 30,
+                muestras: 0
+            },
+            callao:{
+                infectados:25,
+                muestras: 0
+            },
+            arequipa:{
+                infectados:26,
+                muestras: 0,
+            },
+            huanuco:{
+                infectados: 6,
+                muestras: 0
+            },
+            piura:{
+                infectados: 21,
+                muestras: 0
+            },
+            ancash: {
+                infectados: 6,
+                muestras: 0
+            },
+            cuzco: {
+                infectados: 26,
+                muestras: 0,
+            },
+            ica:{
+                infectados:5, 
+                muestras: 0,
+            },
+            lalibertad: {
+                infectados: 19,
+                muestras: 0
+            },
+            loreto:{
+                infectados: 58,
+                muestras: 0
+            },
+            madrededios:{
+                infectados: 1,
+                muestras: 0
+            },
+            junin: {
+                infectados: 17,
+                muestras: 0
+            },
+            tumbes: {
+                infectados: 10, 
+                muestras: 0
+            },
+            sanmartin: {
+                infectados: 6, 
+                muestras: 0,
+            },
+            cajamarca: {
+                infectados: 1,
+                muestras: 0
+            },
+            pasco: {
+                infectados: 1,
+                muestras: 0
+            }
+        }
+    }
+
+
+
+};
+
+
+
+const cities = {
+    'pe-ic': { nombre:'ica', n: 850000 },
+    'pe-cs': { nombre:'cuzco', n: 1205527 },
+    'pe-uc': { nombre:'ucayali', n: 496459 },
+    'pe-md': { nombre:'madrededios', n: 141070 },
+    'pe-sm': { nombre:'sanmartin', n: 813381 },
+    'pe-am': { nombre:'amazonas', n: 379384 },
+    'pe-lo': { nombre:'loreto', n: 883510 },
+    'pe-ay': { nombre:'ayacucho', n: 616176 },
+    'pe-145': { nombre:'callao', n: 994494 },
+    'pe-hv': { nombre:'huancavelica', n: 347639 },
+    'pe-ju': { nombre:'junin', n: 1246038 },
+    'pe-lr': { nombre:'lima', n: 9485405 },
+    'pe-lb': { nombre:'lambayeque', n: 1197260 },
+    'pe-tu': { nombre:'tumbes', n: 224863 },
+    'pe-ap': { nombre:'apurimac', n: 405759 },
+    'pe-ar': { nombre:'arequipa', n: 1382730 },
+    'pe-cl': { nombre:'puno', n: 1172697 },
+    'pe-mq': { nombre:'moquegua', n: 174863 },
+    'pe-ta': { nombre:'tacna', n: 329639 },
+    'pe-an': { nombre:'ancash', n: 1083519 },
+    'pe-cj': { nombre:'cajamarca', n: 1341012 },
+    'pe-hc': { nombre:'huanuco', n: 721047 },
+    'pe-3341': { nombre:'', n: 0 },
+    'pe-ll': { nombre:'lalibertad', n: 1778080 },
+    'pe-pa': { nombre:'pasco', n: 254065 },
+    'pe-pi': { nombre:'piura', n: 1856809 }
+}
+
+let cities2 = {};
+for ( c in cities ){
+    cities2[cities[c].nombre] = { code:c, n:cities[c].n };
+}
---
+++ js/process.js
@@ -0,0 +1,90 @@
+            //const chart = document.getElementById('myChart').getContext('2d');
+
+            let today = Object.keys(data).pop();
+            
+            $(".full-container .stats .muestras").html(data[today].muestras);
+            $(".full-container .stats .infectados").html(data[today].infectados);
+            $(".full-container .stats .muertos").html(data[today].muertos);
+
+            $(".title .today").text(today);
+
+
+            let data_country_day  = {
+                infectados: [],
+                incremento: [],
+            };
+            let yesterday = undefined;
+
+            for ( day in data ){
+                if ( yesterday == undefined ) yesterday = day;
+                
+                data_country_day.infectados.push ( data[day].infectados );
+                data_country_day.incremento.push( data[day].infectados - data[yesterday].infectados );
+                yesterday = day;
+            }
+
+
+            Highcharts.chart('chartInfectados', {
+                title: 'Infectados',
+                series: [
+                    {
+                        name: '# Casos',
+                        data: Object.values(data_country_day.infectados),
+                    },{
+                        name: '# Nuevos Casos', 
+                        data: Object.values(data_country_day.incremento),
+                    }
+                ]
+            });
+
+
+
+            Highcharts.chart('pieStats', {
+                chart: {
+                    type: 'pie'
+                },
+                title: 'Infectados',
+                series: [{
+                    name: 'Stats',
+                    colorByPoint: true,
+                    data:[
+                    {
+                        name: 'Muestras',
+                        y: data[today].muestras,
+                    },{
+                        name: 'Infectados', 
+                        y: data[today].infectados,
+                    },{
+                        name: 'Muertos',
+                        y: data[today].muertos,
+                    }
+                ]}]
+            });
+
+
+            let mapdata = [];
+            for ( departamento in data[today].pordepartamento ){
+                let n = cities2[departamento].n;
+                let infectados = data[today].pordepartamento[departamento].infectados;
+                let percentage = Math.round( ( infectados / Math.round(n/1000) ) * 10000 )/100;
+                console.log(departamento, Math.round(n/1000), infectados, percentage );
+                mapdata.push([ cities2[departamento].code, percentage ]);
+            }
+
+            Highcharts.mapChart('map', {
+                chart: {
+                    map: 'countries/pe/pe-all'
+                }, 
+                title: { text: 'Casos por Region / Poblacion' },
+                series: [{
+                    data: mapdata,
+                    name: 'Infectados'
+                }],
+                colorAxis: {
+                    min: 0,
+                    max: 10,
+                    stops:[[0, '#F1EEF6'], [0.65, '#900037'], [1, '#500007']],
+                    
+                }
+            });
+
